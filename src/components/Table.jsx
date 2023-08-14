af49841e77f68ef8bfe1252e7e9005196ddd78ac
Just-Working8888 <justworking8888@gmail.com>
2023-08-14 17:28:55 +0600

    rest32

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -34,9 +34,7 @@ const MessageTable = ({ data, onDelete }) => {
       title: "Actions",
       dataIndex: "id", // Assume you have an "id" field in your data for each message
       key: "actions",
-      render: (id) => (
-        <button onClick={() => onDelete(id)}>Удалить</button>
-      ),
+      render: (id) => <button onClick={() => onDelete(id)}>Удалить</button>,
     },
   ];
 
@@ -48,6 +46,12 @@ const MessageTable = ({ data, onDelete }) => {
         pagination={false}
         rowKey="id" // Assuming "id" is the unique identifier for each message
       />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi doloribus
+      odit minima laboriosam eligendi non, qui fugit deserunt totam sed esse
+      blanditiis dolore incidunt veniam natus animi sunt fuga harum. Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Debitis assumenda vero esse
+      aliquam ratione eius. Quia sed similique deleniti corporis libero unde!
+      Facilis sequi eos est itaque voluptates incidunt facere.
     </>
   );
 };
