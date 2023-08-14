5b4985e27e1de2873e4a72a2461f4f73e4f72a52
Just-Working8888 <justworking8888@gmail.com>
2023-08-14 21:07:48 +0600

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders all sections', () => {
-//   const { getByTestId } = render(<App />);
-
-  // Verify that each section/component is rendered
-//   expect(getByTestId('header')).toBeInTheDocument();
-//   expect(getByTestId('services')).toBeInTheDocument();
-//   expect(getByTestId('work')).toBeInTheDocument();
-//   expect(getByTestId('workProcess')).toBeInTheDocument();
-//   expect(getByTestId('newsletter')).toBeInTheDocument();
-//   expect(getByTestId('about')).toBeInTheDocument();
-//   expect(getByTestId('testimonials')).toBeInTheDocument();
-//   expect(getByTestId('contact')).toBeInTheDocument();
-//   expect(getByTestId('footer')).toBeInTheDocument();
-});
+test("renders all sections", () => {});
