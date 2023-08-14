6a14de80e85e49156fab09b2b4e6455fdc9da936
Just-Working8888 <justworking8888@gmail.com>
2023-08-14 21:38:55 +0600

    adadadad

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./Footer.css";
 import { FaInstagram, FaTelegram } from "react-icons/fa";
 import { motion } from "framer-motion";
--- src/pages/Meseges.jsx
+++ src/pages/Meseges.jsx
@@ -1,37 +1,11 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import MessageTable from "../components/Table";
+import React from "react";
 
 const Messeges = () => {
-  const [messages, setMessages] = useState([]);
 
-  useEffect(() => {
-    fetchMessages();
-  }, []);
-
-  const fetchMessages = async () => {
-    try {
-      const response = await axios.get("http://localhost:5000/messages");
-      setMessages(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error fetching messages:", error);
-    }
-  };
-
-  const handleDeleteMessage = async (id) => {
-    try {
-      await axios.delete(`http://localhost:5000/messages/${id}`);
-      fetchMessages(); 
-      // Обновляем список сообщений после удаления
-    } catch (error) {
-      console.error("Error deleting message:", error);
-    }
-  };
 
   return (
     <div>
-      <MessageTable data={messages} onDelete={handleDeleteMessage} />
+
     </div>
   );
 };
